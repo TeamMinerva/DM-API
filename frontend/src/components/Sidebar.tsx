ebc933c34c817b7d5395f9182899868ca6f2b15b
Fernanda <fernandafcbpereira@gmail.com>
2026-03-17 17:25:32 -0300

    feat: troca cor da topbar e ajusta padding da sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -7,27 +7,27 @@ export default function Sidebar() {
 
   return (
     <aside className="flex h-screen">
-      <div className="w-24 bg-[#202AD0] rounded-r-[2rem] flex flex-col items-center pt-20 gap-8">
+      <div className="w-28 bg-[#202AD0] rounded-r-[0.5rem] flex flex-col items-center pt-40 gap-8">
         <button
           onClick={() => setIsOpen(!isOpen)}
          
-          className="text-white"
+          className="text-white pt-10"
         >
-          <LayoutGrid size={29}  />
+          <LayoutGrid size={32} />
         </button>
 
         <button className="text-white">
-          <SlidersHorizontal size={29} />
+          <SlidersHorizontal size={32} />
         </button>
       </div>
 
       {isOpen && (
 
-        <div className="w-36 bg-[#2350DE] color rounded-r-[2rem] flex flex-col pt-20 px-4 text-white ">
+        <div className="w-40 bg-[#2350DE] color rounded-r-[2rem] flex flex-col pt-20 px-8 text-white ">
 
           <img src = {logo} alt="logo da DM" className="w-12 mb-12 self-center" />
 
-          <nav className="flex flex-col gap-7 text-sm">
+          <nav className="flex flex-col gap-9 text-base pt-5" >
             <a href="#" className="hover:opacity-80">Dashboard</a>
             <a href="#" className="hover:opacity-80">Mercado</a>
             <a href="#" className="hover:opacity-80">Risco</a>
--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,11 +1,11 @@
 export default function Topbar() {
   return (
     <header className="flex justify-end gap-3 p-6">
-      <button className="px-4 py-1 rounded-full border border-[#202AD0] text-[#202AD0] text-sm hover:bg-[#202AD0] hover:text-white transition">
+      <button className="px-4 py-1 rounded-full border border-[#2350DE] text-[#2350DE] text-base hover:bg-[#2350DE] hover:text-white transition">
       Fonte BCB
      </button>
 
-      <button className="px-4 py-1 rounded-full bg-[#202AD0] text-white text-sm">
+      <button className="px-4 py-1 rounded-full bg-[#2350DE] text-white text-base">
         Acumulado 2024
       </button>
     </header>
