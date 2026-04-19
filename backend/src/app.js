a661e71fcc59235c1189643be0fa50e629015605
Guilherme Rosa <guilherme_rozza@hotmail.com>
2026-04-19 19:33:55 -0300

    feat: criação do endpoint get das taxas de inadimplência do heatmap

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ const kpisRoutes = require("./routes/indicadoresKPI");
 const indicadoresEstadosRoutes = require('./routes/indicadoresEstados');
 const indicadoresRegioesRoutes = require('./routes/indicadoresRegioes');
 const regioesCarteira = require('./routes/regioesCarteira')
+const heatmapRoutes = require('./routes/indicadoresHeatmap')
 
 const app = express();
 
@@ -13,10 +14,11 @@ app.use(express.json());
 
 // rotas
 app.use("/indicadores-nacionais", indicadoresRoutes);
-app.use("/", kpisRoutes);
+app.use("/", kpisRoutes); 
 app.use("/", indicadoresEstadosRoutes);
 app.use('/regioes', indicadoresRegioesRoutes);
 app.use("/regioes", regioesCarteira)
+app.use("/heatmap", heatmapRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "API Node.js rodando 🚀" });
