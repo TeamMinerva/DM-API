4c7d6320199dfc4e2063517b1e505429cfeb0a3c
Caio César <caiocesar.santos1511@gmail.com>
2026-04-25 14:02:11 -0300

    fix: adicionado configurações para o frontend atualizar no runtime

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Permite que o Vite aceite conexões de fora do container
+    host: true, 
+    strictPort: true,
+    port: 5173,
+    watch: {
+      // Força o monitoramento de arquivos via "polling"
+      // Necessário para volumes montados via Docker no Windows
+      usePolling: true,
+    },
+  },
 })
